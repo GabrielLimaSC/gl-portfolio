356120f25840b31a32fe0e921e31ca9a6f843088
Gabriel Cavalcanti <61714189+GabrielLimaSC@users.noreply.github.com>
2026-03-11 10:57:54 -0300

    🐛 fix: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import Desktop from './components/Desktop/Desktop'
 import Taskbar from './components/Taskbar/Taskbar'
 import Window from './components/Window/Window'
 import DesktopIcon from './components/DesktopIcon/DesktopIcon'
+import AuthApp from './components/apps/AuthApp/AuthApp'
 
 const App = () => {
+  const [currentToken, setCurrentToken] = useState<string | null>(null)
+
   return (
     <>
       <Desktop />
@@ -12,10 +16,10 @@ const App = () => {
       <DesktopIcon id="portfolio" label={"PORTFOLIO"} emoji="📁" position={{ x: 116, y: 28 }} />
 
       <Window id="auth">
-        <p>Auth aqui</p>
+        <AuthApp onTokenGenerated={(token) => setCurrentToken(token)} />
       </Window>
       <Window id="portfolio">
-        <p>Portfolio aqui</p>
+        <p style={{ color: 'white' }}>Token atual: {currentToken ?? 'nenhum'}</p>
       </Window>
 
       <Taskbar />
